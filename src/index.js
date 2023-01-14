41f8df2fabe86da009f54590882c0f8df98ce6ef
Antonio Pons <toninopons@gmail.com>
2023-01-14 11:26:56 +0100

    done.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './state/Store';
+import { incrementCounter } from './state/CounterState';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
+
+store.subscribe(() => console.log('redux state: ' + store.getState()));
+store.dispatch(incrementCounter(5));
+store.dispatch(incrementCounter(5));
+store.dispatch(incrementCounter(5));
+store.dispatch(incrementCounter(5));
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/state/Store.js
@@ -0,0 +1,4 @@
+import { createStore } from "redux";
+import { counterReducer, incrementCounter } from "./CounterState";
+
+export const store = createStore(counterReducer);
